a2d962c2c596045745f38847fdd6eaacbf08578c
balancy <balancy@gmail.com>
2021-05-20 13:07:17 +0200

    Move all serializers from views to utils

--- education/views.py
+++ education/views.py
@@ -1,41 +1,8 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
-from education.models import Article, Category, Tag
-
-
-def serialize_tag_with_count(tag):
-    return {
-        "title": tag.title,
-        "articles_count":
-            tag.articles_count if hasattr(tag, "articles_count") else 0,
-    }
-
-
-def serialize_tag(tag):
-    return {
-        "title": tag.title,
-    }
-
-
-def serialize_article(article):
-    return {
-        "title": article.title,
-        "published_at": article.published_at,
-        "author": article.author,
-        "text": article.text,
-        "tags": [serialize_tag(tag) for tag in article.tags.all()],
-    }
-
-
-def serialize_category(category):
-    return {
-        "title": category.title,
-        "articles": [
-            serialize_article(article) for article
-            in category.articles.filter(published_at__isnull=False)
-        ],
-    }
+from education.models import Category, Tag
+from education.utils import serialize_category, serialize_tag_with_count
 
 
 def articles_list(request: HttpRequest):
